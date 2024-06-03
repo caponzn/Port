22b67dd77bfa8be412f334c3908c27c2f85ede3b
Ricardo Glória <99296536+caponzn@users.noreply.github.com>
2024-06-03 20:56:11 -0300

    65% feito

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -10,11 +10,17 @@ const Hero = () => {
         backgroundColor: theme.palette.primary.main,
         height:"100vh",
         display:"flex",
-        alignItems:"center"
+        alignItems:"center",
+        [theme.breakpoints.up('xs')]:{ // <= Mobile
+            paddingTop: "100px"
+        },
+        [theme.breakpoints.up('md')]:{ // >= Mobile
+            paddingTop: "100px"
+        }
     }))
 
     const StyledImg = styled("img")(()=>({
-        width:"80%",
+        width:"75%",
         borderRadius:"50%",
         border: `1px solid  ${theme.palette.primary.contrastText}`,
     }))
